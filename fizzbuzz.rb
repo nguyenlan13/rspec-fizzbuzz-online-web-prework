d48744f361c956134b6d3908a686d4630434263d
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-16 05:54:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,7 @@
 def fizzbuzz(int)
   
   
+  
   if int %3 ==0
 return "Fizz"
 
@@ -13,8 +14,6 @@ elsif int %5 ==0
 return "Buzz"
 
 
-elsif (int %3==0) && (int %5==0)
-return "FizzBuzz"
 
 else 
   return
